e2abfb3b9ebe07288bbaa65b080da6e4d362cf41
Jonathan Serafini <jonathan@serafini.ca>
2016-08-11 13:07:12 -0400

    Update attributes

--- attributes/samson.rb
+++ attributes/samson.rb
@@ -1,14 +1,19 @@
+# User/Group which both owns the Samson files and is assumed when running the
+# _samson_ service.
 default['samson']['user'] = 'samson'
 default['samson']['group'] = 'samson'
 
+# Whether to create the user and group.
+default['samson']['manage_user'] = true
+
+# Root directory under which Samson is deployed in an atomic manner.
 default['samson']['root_dir'] = '/var/www/samson'
 
-default['samson']['source']['manage_user'] = true
-
+# Name of the _samson_ service resource.
 default['samson']['service']['name'] = 'service[samson]'
 
+# Version of ruby managed by rbenv.
 default['samson']['ruby']['version'] = '2.3.1'
-default['samson']['ruby']['manage_ruby'] = true
 
+# Version of node managed by nvm.
 default['samson']['node']['version'] = '6.1.0'
-default['samson']['node']['manage_node'] = true
