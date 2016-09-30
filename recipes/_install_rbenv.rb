98f88e764d6f48d7792217e652f054c176b334f5
Francois Lapierre Messier <francois.lapierremessier@lightspeedhq.com>
2016-09-30 09:49:55 +0200

    Work in progress for the refactor

--- recipes/_install_rbenv.rb
+++ recipes/_install_rbenv.rb
@@ -1,10 +1,14 @@
 include_recipe 'ruby_rbenv::default'
-include_recipe 'ruby_rbenv::system_install'
-include_recipe 'ruby_build::default'
 
-rbenv_ruby node['samson']['ruby']['version'] do
-  user node['samson']['user']
-end
+node.default['rbenv']['user_installs'] = [
+  { 'user'    => node['samson']['user'],
+    'home'    => node['samson']['root_dir'],
+    'rubies'  => [node['samson']['ruby']['version']]
+  }
+]
+
+include_recipe 'ruby_build::default'
+include_recipe 'ruby_rbenv::user'
 
 rbenv_gem 'bundle' do
   rbenv_version node['samson']['ruby']['version']
