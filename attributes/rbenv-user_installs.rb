55bf3b83b45926d1ea8c094040a348c757e7502c
Francois Lapierre Messier <francois.lapierremessier@lightspeedhq.com>
2016-10-10 11:12:36 +0200

    Fixed attribute definition

--- attributes/rbenv-user_installs.rb
+++ attributes/rbenv-user_installs.rb
@@ -1,4 +1,4 @@
-node.default['rbenv']['user_installs'] = [
+default['rbenv']['user_installs'] = [
   { 'user'    => node['samson']['user'],
     'home'    => node['samson']['root_dir'],
     'rubies'  => [node['samson']['ruby']['version']]
