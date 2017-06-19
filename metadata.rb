9cd546051020c3a96cfe190c2696f1777f94601d
Michiel Rensen <michiel.rensen@lightspeedhq.com>
2017-06-19 15:36:03 +0200

    Updated metadata, bumped version and using newer versions of some deps

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,16 @@
 name 'samson'
-maintainer 'Jonathan Serafini'
-maintainer_email 'jonathan@serafini.ca'
-license 'apachev2'
+maintainer 'Omni DevOps'
+maintainer_email 'pg.omni.devops@lightspeedhq.com'
+license 'Apache-2.0'
 description 'Installs/Configures samson'
 long_description 'Installs/Configures samson'
-version '0.3.0'
-depends 'common_deploy',  '~> 0.2.0'
+version '1.0.0'
+chef_version     '~> 12.7.2'
+
+source_url       'https://github.com/lightspeedretail/chef-samson'
+issues_url       File.join(source_url, 'issues')
+
+depends 'common_deploy',  '~> 1.1.0'
 depends 'nvm',            '~> 0.1.7'
-depends 'ruby_rbenv',     '~> 1.0.0'
-depends 'ruby_build',     '~> 1.0.0'
+depends 'ruby_rbenv',     '~> 1.0.1'
+depends 'ruby_build',     '~> 1.1.0'
