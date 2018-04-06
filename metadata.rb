0112d2b779d26383e0c53b9f55c1b0c66c8b098a
Michiel Rensen <michiel.rensen@lightspeedhq.com>
2018-04-06 17:51:13 +0200

    Bumped v1.0.0 + fixed dependencies
    Signed-off-by: Michiel Rensen <michiel.rensen@lightspeedhq.com>

--- metadata.rb
+++ metadata.rb
@@ -4,15 +4,15 @@ maintainer_email 'pg.omni.devops@lightspeedhq.com'
 license          'Apache 2.0'
 description      'Installs/Configures samson'
 long_description 'Installs/Configures samson'
-version          '0.4.0'
+version          '1.0.0'
 chef_version     '~> 12.7.2'
 
-source_url       'https://github.com/lightspeedretail/chef-cml_role_backofficev2'
+source_url       'https://github.com/lightspeedretail/chef-samson'
 issues_url       File.join(source_url, 'issues')
 
 supports 'ubuntu 14'
 
-depends 'common_deploy',  '~> 1.1.0'
+depends 'common_deploy',  '~> 0.2.0'
 depends 'nvm',            '~> 0.1.7'
-depends 'ruby_rbenv',     '~> 1.0.0'
-depends 'ruby_build',     '~> 1.0.0'
+depends 'ruby_rbenv',     '~> 1.1.0'
+depends 'ruby_build',     '~> 1.1.0'
