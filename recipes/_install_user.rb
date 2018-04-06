dc5b82c15f8d18238fc124f003792bca517b6346
Michiel Rensen <michiel.rensen@lightspeedhq.com>
2018-04-06 17:52:51 +0200

    Make sure to own the homefolder as well
    Signed-off-by: Michiel Rensen <michiel.rensen@lightspeedhq.com>

--- recipes/_install_user.rb
+++ recipes/_install_user.rb
@@ -12,6 +12,16 @@ user node['samson']['user'] do
   only_if { node['samson']['manage_user'] }
 end
 
+directory 'samson home_dir' do
+  owner node['samson']['user']
+  group node['samson']['group']
+  path node['samson']['home_dir']
+  recursive true
+  not_if do
+    ::File.directory?(node['samson']['home_dir'])
+  end
+end
+
 # Optionaly create the root directory for situations where the parent
 # directories have not yet been created. For situations where we may want to
 # deploy into an existing directory, ignore this step.
